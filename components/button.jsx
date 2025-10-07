13f2e72c2265064bc8e23840528cfae1f1b49dfb
aman2712 <khanamankhan1973@gmail.com>
2025-10-07 15:04:21 +0400

    unused files deleted

--- components/button.jsx
+++
@@ -1,5 +0,0 @@
-export default function Button({ children, classes }) {
-    return (
-        <button className={`bg-white px-4 py-2 w-44 font-aktivgrotesk cursor-pointer hover:bg-white/70 font-semibold border border-white transition duration-300 ${classes} `}>{children}</button>
-    )
-}
